96687a04dbcb1d6b66db79589b259a22a1d9727d
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-21 12:24:50 +0300

    2nd video done

--- Capturing Mouse Movement/main.js
+++ Capturing Mouse Movement/main.js
@@ -1,44 +1,75 @@
+var pupilsHTMLColletion = document.getElementsByClassName("pupil");
+
+console.log("pupilsHTMLColletion", pupilsHTMLColletion);
+// console.log("pupilsArray", pupilsArray);
+
 var input = {
   mouseX: {
     start: 0,
-    end: window.innerWidth ,
+    end: window.innerWidth,
     current: 0,
   },
   mouseY: {
     start: 0,
-    end: window.innerHeight ,
+    end: window.innerHeight,
     current: 0,
-  }
+  },
 };
 
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
-var handleMouseMove = function (event) {
-  input.mouseX.current = event.clientX;
-  input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
-  
-  input.mouseY.current = event.clientY;
-  input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
-  
-  // if(input.mouseX.fraction > 1) {
-  //   input.mouseX.fraction = 1;
-  // }
-  // if(input.mouseX.fraction < 0) {
-  //   input.mouseX.fraction = 0;
-  // }
+var output = {
+  x: {
+    start: -75,
+    end: 75,
+    current: 0,
+  },
+  y: {
+    start: -75,
+    end: 75,
+    current: 0,
+  },
+};
 
-  console.log('fraction X', input.mouseX.fraction);
-  console.log('fraction Y', input.mouseY.fraction);
-}
+output.x.range = output.x.end - output.x.start;
+output.y.range = output.y.end - output.y.start;
+
+var handleMouseMove = function (event) {
+
+  let pupilsArray = Array.from(pupilsHTMLColletion);
+  
+  input.mouseX.current = event.clientX;
+  input.mouseX.fraction =
+    (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
+
+  input.mouseY.current = event.clientY;
+  input.mouseY.fraction =
+    (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
+
+  output.x.current = output.x.end - (input.mouseX.fraction * output.x.range);
+  output.x.opposite = output.x.start + (input.mouseX.fraction * output.x.range);
+  
+  output.y.current = output.y.end - (input.mouseY.fraction * output.y.range);
+  output.y.opposite = output.y.start + (input.mouseY.fraction * output.y.range);
+
+  pupilsArray.forEach(function (pupil, k) {
+    if(k === 0) {
+      pupil.style.transform = 'translate('+output.x.opposite+'px, '+output.y.opposite+'px)';
+    } else {
+      pupil.style.transform = 'translate('+output.x.current+'px, '+output.y.current+'px)';
+    }
+  });
+
+};
 
 var handleResize = function () {
   input.mouseX.end = window.innerWidth;
   input.mouseX.range = input.mouseX.end - input.mouseX.start;
-  
+
   input.mouseY.end = window.innerHeight;
   input.mouseY.range = input.mouseY.end - input.mouseY.start;
-}
+};
 
-window.addEventListener('mousemove', handleMouseMove);
-window.addEventListener('resize', handleResize);
+window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("resize", handleResize);
