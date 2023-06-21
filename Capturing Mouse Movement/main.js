a794f397a632e4ac283a86c7079ab5e3b3248f16
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-06-21 10:34:47 +0300

    first commit

---
+++ Capturing Mouse Movement/main.js
@@ -0,0 +1,16 @@
+var input = {
+  mouseX: {
+    start: 0,
+    end: window.innerWidth,
+    current: 0,
+  },
+  mouseY: {}
+};
+
+input.mouseX.range = input.mouseX.end - input.mouseX.start;
+
+var handleMouseMove = function (event) {
+  console.log('event', event);
+}
+
+window.addEventListener('mousemove', handleMouseMove);
